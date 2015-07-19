48d81301b1b167e07f5dfab3969f3d9b6c19588a
Jonathan Wexler <Jwexman@gmail.com>
2015-07-18 21:52:29 -0400

    not working messages

---
+++ server/accounts.js
@@ -0,0 +1,9 @@
+// ServiceConfiguration.configurations.remove({
+//   service: "github"
+// });
+
+// ServiceConfiguration.configurations.insert({
+//   service: 'github',
+//   appId: '4b7a40dce334c1332f4c',
+//   secret: '6283ffaeb3b4793c6bc0226312d9db89c211dcee'
+// });
